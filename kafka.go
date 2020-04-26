ccb2d408081af4f794642ac07af29f29e4ac02f8
Albin CR <Albinzr@gmail.com>
2020-04-26 17:06:49 +0530

    update

--- kafka.go
+++ kafka.go
@@ -81,7 +81,7 @@ func (c *Config) Reader(readMessageCallback func(reader *kafka.Reader, m kafka.M
 		recivedTime := time.Now().Nanosecond()
 		fmt.Println("message reciving time gap:", recivedTime-prevTime)
 		prevTime = recivedTime
-		readMessageCallback(r, m)
+		go readMessageCallback(r, m)
 	}
 
 }
