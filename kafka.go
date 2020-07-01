ae5549de1747d374f086d78d1365cc701f02399c
Albin CR <Albinzr@gmail.com>
2020-07-01 23:33:32 +0530

    update

--- kafka.go
+++ kafka.go
@@ -109,7 +109,7 @@ func (c *Config) WriteBulk(message string, callback func(bool)) {
 }
 
 //Reader :- read msg from kafka
-func (c *Config) GetReader(readMessageCallback func(reader *kafka.Reader, m kafka.Message)) {
+func (c *Config) Reader(readMessageCallback func(reader *kafka.Reader, m kafka.Message)) {
 
 	r := kafka.NewReader(kafka.ReaderConfig{
 		Brokers:   []string{c.URL},
