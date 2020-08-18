81c436840178591c0afcf65e4415bb50b954e31e
Albin CR <Albinzr@gmail.com>
2020-08-18 20:16:37 +0530

    i

--- kafka.go
+++ kafka.go
@@ -163,7 +163,8 @@ func (c *Config) ReaderWithLimit(limit int, readMessageCallback func(reader *kaf
 	ctx := context.Background()
 	for i := 0; i <= limit; i++ {
 		fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", i)
-		m, err := r.FetchMessage(ctx)
+		// m, err := r.FetchMessage(ctx)
+		m, err := r.ReadMessage(ctx)
 		if err != nil {
 			break
 		}
